cc1057d2b73cf5c5cf1ee9128a79d411f4c70000
Vlasov Artem <avlasov@selectica.com>
2017-07-11 18:07:44 +0300

    Add test resources creation data and sql schemas and test log4j2

---
+++ src/test/resources/scripts/book/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO book VALUES
+  (1, 'Murder on the Orient Express', '9788370231743', 'CRIME'),
+  (2, 'Fight Club', '9788360979426', 'SATIRE'),
+  (3, 'Before the Law', '978544671809', 'NOVEL'),
+  (4, 'The Catcher in the Rye', '978557571809', 'NOVEL'),
+  (5, 'The Great Gatsby', '9780230035287','HISTORICAL_FICTION');
