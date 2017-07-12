2e36012ef4ea305af5cc783ce700c9aeabb529a8
Vlasov Artem <avlasov@selectica.com>
2017-07-12 23:32:53 +0300

    Remove useless scripts

--- src/test/resources/scripts/author/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO author VALUES
-  (1, 'Chuck', 'Palahniuk', 'MALE', '1962-02-21'),
-  (2, 'J.D.', 'Salinger', 'MALE', '1919-01-01'),
-  (3, 'F. Scott', 'Fitzgerald', 'MALE', '1896-09-24'),
-  (4, 'Franz', 'Kafka', 'MALE', '1883-06-03'),
-  (5, 'Agatha', 'Christie', 'FEMALE', '1890-09-15');
--- src/test/resources/scripts/book/drop-data.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM author_book;
-DELETE FROM reward;
-DELETE FROM book;
-DELETE FROM author;
