5caaf4c94d3f31649fc27bd477710bc2bd89e571
Vlasov Artem <avlasov@selectica.com>
2017-07-12 21:20:27 +0300

    Add SecurityConfig, update Controller with MockUser

---
+++ src/main/java/org/avlasov/qrok/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.avlasov.qrok.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by artemvlasov on 12/07/2017.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").hasRole("USER");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("qrok").password("qrok-password").roles("USER");
+    }
+
+}
--- src/test/java/org/avlasov/qrok/controller/AuthorControllerTest.java
+++ src/test/java/org/avlasov/qrok/controller/AuthorControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest
 @ContextConfiguration(classes = AuthorController.class)
+@WithMockUser(username = "qrok", password = "qrok-password")
 public class AuthorControllerTest {
 
     @MockBean
